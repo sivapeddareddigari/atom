7c6cd499783ecc9ceaf19afb079d31c5bdb46392
Pirogov, Timofey <timofey.pirogov@accenture.com>
2021-03-03 14:26:26 -0600

    Add StringHelper.java

---
+++ library-common/src/main/java/automation/library/common/StringHelper.java
@@ -0,0 +1,32 @@
+package automation.library.common;
+
+public class StringHelper {
+    private StringHelper() {
+    }
+
+    /**
+     * @param src  - source string
+     * @param what - what to find
+     * @return true if what is in source with case ignorant
+     */
+    public static boolean containsIgnoreCase(String src, String what) {
+        final int length = what.length();
+        if (length == 0)
+            return true; // Empty string is contained
+
+        final char firstLo = Character.toLowerCase(what.charAt(0));
+        final char firstUp = Character.toUpperCase(what.charAt(0));
+
+        for (int i = src.length() - length; i >= 0; i--) {
+            // Quick check before calling the more expensive regionMatches() method:
+            final char ch = src.charAt(i);
+            if (ch != firstLo && ch != firstUp)
+                continue;
+
+            if (src.regionMatches(true, i, what, 0, length))
+                return true;
+        }
+        return false;
+    }
+
+}

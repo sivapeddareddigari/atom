75269abff0a90b0dd7528acd0ea1882c78145f2d
Shiva Reddy <sivapeddareddigari@gmail.com>
2021-11-02 15:42:40 -0400

    support added

--- sample-cucumber-selenium/src/test/java/pageobjects/LoginBankingPage.java
+++ sample-cucumber-selenium/src/test/java/pageobjects/LoginBankingPage.java
@@ -3,6 +3,11 @@ package pageobjects;
 import automation.library.cucumber.selenium.BaseSteps;
 import automation.library.selenium.exec.BasePO;
 import org.openqa.selenium.By;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+
 
 public class LoginBankingPage extends BasePO {
 
@@ -12,12 +17,36 @@ public class LoginBankingPage extends BasePO {
     private static final By btnAdvanced = By.id("details-button");
     private static final By lnkProceedLink = By.id("proceed-link");
 
-    public void login(){
-        performDriverOperation("goto", "https://localhost:8443/bank/login");
+    public void login() throws IOException{
+        String url = "https://" + getHostIP().trim() + ":8443/bank/login";
+        performDriverOperation("goto", url);
         performElementOperation("click", "button", "", "Advanced", btnAdvanced);
         performElementOperation("click", "button", "", "Proceed Link", lnkProceedLink);
         performElementOperation("sendKeys", "text", "jsmith@demo.io", "userName", txtUserName );
         performElementOperation("sendKeys", "text", "Demo123!", "userName", txtPassword );
         performElementOperation("click", "button", "", "SignIn", btnSignIn);
+    }
+
+    public String getHostIP() throws IOException {
+
+        String hostIP;
+        String resultStr = "";
+        hostIP=InetAddress.getLocalHost().getHostAddress();
+        if (System.getProperty("os.name").equalsIgnoreCase("linux"))
+        {
+            try {
+                InputStream fis = new FileInputStream("/home/ubuntu/project/TTPObjectSpy/hostIP.txt");
+                InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+                BufferedReader br = new BufferedReader(isr);
+                hostIP = br.readLine();
+                hostIP = hostIP.replaceAll("[^a-zA-Z0-9\\.+]", "");
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+        return hostIP;
     }
 }
